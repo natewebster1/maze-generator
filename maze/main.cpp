24cb16dc63ea487826ba55ec1eae28149c15a625
Nate Webster <natewebster@g.ucla.edu>
2022-08-19 22:57:18 -0700

    Updated main to prompt for user input to make new mazes

--- maze/main.cpp
+++ maze/main.cpp
@@ -14,8 +14,30 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    Maze A(20,20,"bottom","top");
-    A.display();
-    A.display_solution();
+
+    while (true) {
+        int rows, columns;
+        std::string start, end, solve;
+        
+        std::cout << "Make a new maze!\n" << "\n";
+        std::cout << "How many rows?: ";
+        std::cin >> rows;
+        std::cout << "How many columns?: ";
+        std::cin >> columns;
+        std::cout << "Where should the maze start (top, bottom, left, or right)?: ";
+        std::cin >> start;
+        std::cout << "Where should the maze end (top, bottom, left, or right)?: ";
+        std::cin >> end;
+        
+        Maze M(rows, columns, start, end);
+        
+        M.display();
+        std::cout << "Show the solution (yes or no)?: ";
+        std::cin >> solve;
+        if (solve == "yes")
+            M.display_solution();
+        
+        std::cout << "\n";
+    }
     return 0;
 }
